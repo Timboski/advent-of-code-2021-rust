abc2a1c93beadc7cc4b9551a8a504f84f0dbe806
Tim Boyle <timboboyle@yahoo.co.uk>
2021-12-08 10:38:22 +0000

    Add test for puzzle 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -7,11 +7,14 @@ fn main() {
     let file = std::fs::read_to_string(path).expect("file not found!");
     let lines: Vec<&str> = file.lines().collect();
 
-    let res1 = decode_diagnostic(&lines);
+    let res1 = decode_power_consumption(&lines);
     println!("Puzzle 1 result: {}", res1);
+
+    let res2 = decode_life_support_rating(&lines);
+    println!("Puzzle 2 result: {}", res2);
 }
 
-fn decode_diagnostic(diagnostic: &[&str]) -> u32 {
+fn decode_power_consumption(diagnostic: &[&str]) -> u32 {
     let mut total: u32 = 0;
     let num_bits = diagnostic[0].len();
     for i in 0..num_bits {
@@ -23,6 +26,10 @@ fn decode_diagnostic(diagnostic: &[&str]) -> u32 {
     let mask: u32 = ((1 << num_bits) - 1).try_into().unwrap();
     let inv = !total & mask;
     total * inv
+}
+
+fn decode_life_support_rating(diagnostic: &[&str]) -> u32 {
+    0
 }
 
 fn get_bit(i: usize, diagnostic: &[&str]) -> bool {
@@ -54,17 +61,31 @@ fn test_get_bit() {
 }
 
 #[test]
-fn test_example_diagnostic_from_file() {
+fn test_power_consumption_from_example_diagnostic_file() {
     // Arrange
     let path = "/workspaces/advent-of-code-2021-rust/day3/src/test_example";
     let file = std::fs::read_to_string(path).expect("file not found!");
     let lines: Vec<&str> = file.lines().collect();
 
     // Act
-    let res = decode_diagnostic(&lines);
+    let res = decode_power_consumption(&lines);
 
     // Assert
     assert_eq!(res, 198);
+}
+
+#[test]
+fn test_life_support_rating_from_example_diagnostic_file() {
+    // Arrange
+    let path = "/workspaces/advent-of-code-2021-rust/day3/src/test_example";
+    let file = std::fs::read_to_string(path).expect("file not found!");
+    let lines: Vec<&str> = file.lines().collect();
+
+    // Act
+    let res = decode_life_support_rating(&lines);
+
+    // Assert
+    assert_eq!(res, 230);
 }
 
 #[test]
@@ -75,7 +96,7 @@ fn puzzle1_regression() {
     let lines: Vec<&str> = file.lines().collect();
 
     // Act
-    let res = decode_diagnostic(&lines);
+    let res = decode_power_consumption(&lines);
 
     // Assert
     assert_eq!(res, 1071734);
