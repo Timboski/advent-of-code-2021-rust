c2e85780f749bcc027fca15bd13cb2020d02c27d
Tim Boyle <timboboyle@yahoo.co.uk>
2021-12-08 10:19:53 +0000

    Add regression test for puzzle 1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -66,3 +66,17 @@ fn test_example_diagnostic_from_file() {
     // Assert
     assert_eq!(res, 198);
 }
+
+#[test]
+fn puzzle1_regression() {
+    // Arrange
+    let path = "/workspaces/advent-of-code-2021-rust/day3/src/puzzle_input";
+    let file = std::fs::read_to_string(path).expect("file not found!");
+    let lines: Vec<&str> = file.lines().collect();
+
+    // Act
+    let res = decode_diagnostic(&lines);
+
+    // Assert
+    assert_eq!(res, 1071734);
+}
