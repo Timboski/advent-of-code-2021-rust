c389a83edf0a29609920887e2570b2ac7cf89222
Tim Boyle <timboboyle@yahoo.co.uk>
2021-12-08 10:10:15 +0000

    Implement decoder

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,9 +1,54 @@
+use std::process::abort;
+
 fn main() {
     println!("Binary Diagnostic");
 }
 
-fn decode_diagnostic(diagnostic: &[&str]) -> i32 {
-    0
+fn decode_diagnostic(diagnostic: &[&str]) -> u32 {
+    let mut total: u32 = 0;
+    let num_bits = diagnostic[0].len();
+    for i in 0..num_bits {
+        match get_bit(i, diagnostic) {
+            true => {
+                let bit_value = 1 << (num_bits - i - 1);
+                println!("Bit {} = {}", i, bit_value);
+                total += bit_value
+            }
+            false => {}
+        };
+    }
+
+    let mask: u32 = ((1 << num_bits) - 1).try_into().unwrap();
+    let inv = !total & mask;
+    total * inv
+}
+
+fn get_bit(i: usize, diagnostic: &[&str]) -> bool {
+    let sum: i32 = diagnostic
+        .iter()
+        .map(|a| match a.chars().nth(i).unwrap() {
+            '0' => -1,
+            '1' => 1,
+            _ => abort(),
+        })
+        .sum();
+    sum > 0
+}
+
+#[test]
+fn test_get_bit() {
+    // Arrange
+    let diagnostic = ["011", "001", "100"];
+
+    // Act
+    let res0 = get_bit(0, &diagnostic);
+    let res1 = get_bit(1, &diagnostic);
+    let res2 = get_bit(2, &diagnostic);
+
+    // Assert
+    assert!(!res0);
+    assert!(!res1);
+    assert!(res2);
 }
 
 #[test]
